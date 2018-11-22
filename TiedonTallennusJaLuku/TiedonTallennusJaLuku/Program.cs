c1d1a1dd472877cf32f8b3aa15d02a5df3a869b0
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2018-11-22 13:47:28 +0200

    Lisätty aiemman lämpötilan luku tiedostosta

--- TiedonTallennusJaLuku/TiedonTallennusJaLuku/Program.cs
+++ TiedonTallennusJaLuku/TiedonTallennusJaLuku/Program.cs
@@ -11,13 +11,27 @@ namespace TiedonTallennusJaLuku
     {
         static void Main(string[] args)
         {
+            string tiedosto = @"C:\Users\OWNER\Google Drive\PointCollege\Kurssi_05_Ohjelmoinnin jatkokurssi\lämpötila.txt";
+
+            //luetaan tiedostosta aiemmin asetettu lämpötila
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine($"Aiemmin asetettu lämpötila on {aiempiArvo}°");
+            }
+            else
+            {
+                Console.WriteLine("Tervetuloa lämpötilan asettamisohjelmaan!");
+            }
+
+
+
             int lämpötila = 0;
             Console.Write("Anna uusi lämpötila: ");
             string syöte = Console.ReadLine();
             lämpötila = int.Parse(syöte);
 
             //tiedostoon kirjoittaminen
-            string tiedosto = @"C:\Users\OWNER\Google Drive\PointCollege\Kurssi_05_Ohjelmoinnin jatkokurssi\lämpötila.txt";
             File.WriteAllText(tiedosto, lämpötila.ToString());
             Console.WriteLine("Uusi lämpötila tallennettu");
             Console.ReadLine();
